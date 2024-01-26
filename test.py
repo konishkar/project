b003197fca33da807a5595a3f76e2c0e29d26dd3
konishkar <157840298+konishkar@users.noreply.github.com>
2024-01-27 00:46:37 +0530

    Create test.py

---
+++ test.py
@@ -0,0 +1,174 @@
+{
+ "cells": [
+  {
+   "cell_type": "markdown",
+   "id": "a8c38b8d",
+   "metadata": {},
+   "source": [
+    "<h1>Data Science Tools and Ecosystem</h1> "
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "a4ed538f",
+   "metadata": {},
+   "source": [
+    "In this notebook, Data Science Tools and Ecosystem are summarized"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "f688cd62",
+   "metadata": {},
+   "source": [
+    "<body>\n",
+    "<p>\n",
+    "Some of the popular languages that Data Scientists use are: </p>\n",
+    "    <ol>\n",
+    "    <li> Python</li>\n",
+    "    <li> SQL  (Structured Query Language)</li>\n",
+    "    <li> R </li>\n",
+    "    <li> Julia </li>\n",
+    "    </ol>\n",
+    "    </body>"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "578e328d",
+   "metadata": {},
+   "source": [
+    "Some of the commonly used libraries used by Data Scientists include:\n",
+    "<ol>\n",
+    "    <li>matplotlib</li>\n",
+    "    <li>numpy</li>\n",
+    "    <li>pandas</li>\n",
+    "    <li>seaborn</li>\n",
+    "    </ol>"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "18ea5569",
+   "metadata": {},
+   "source": [
+    "<table>\n",
+    "    <tr>\n",
+    "        <th>Data Science Tools</th></tr>\n",
+    "    <tr>\n",
+    "        <td> spyder</td>\n",
+    "    </tr>\n",
+    "        <tr>\n",
+    "        <td> Jupyter Notebooks</td>\n",
+    "    </tr>    <tr>\n",
+    "        <td> RStudio</td>\n",
+    "    </tr>\n",
+    "    </table>"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "3b32d97e",
+   "metadata": {},
+   "source": [
+    "<h3>Below are a few examples of evaluating arithmetic expressions in Python</h3>"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 2,
+   "id": "cb45e67f",
+   "metadata": {},
+   "outputs": [
+    {
+     "data": {
+      "text/plain": [
+       "17"
+      ]
+     },
+     "execution_count": 2,
+     "metadata": {},
+     "output_type": "execute_result"
+    }
+   ],
+   "source": [
+    "(3*4)+5\n",
+    "#This a simple arithmetic expression to mutiply then add integers."
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 3,
+   "id": "b28aea45",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "3.3333333333333335\n"
+     ]
+    }
+   ],
+   "source": [
+    "minutes = 200\n",
+    "hours = minutes / 60\n",
+    "print(hours)\n",
+    "# This will convert 200 minutes to hours by diving by 60."
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "a803aeda",
+   "metadata": {},
+   "source": [
+    "<b>Objectives</b>"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "4c83077d",
+   "metadata": {},
+   "source": [
+    "<p>List popular languages for Data Science.</p>\n",
+    "<ul>\n",
+    "<li>Python</li>\n",
+    "<li>R</li>\n",
+    "<li>SQL</li>\n",
+    "<li>Java</li>\n",
+    "<li>Julia</li>\n",
+    "</ul>"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "5abafd1b",
+   "metadata": {},
+   "source": [
+    "<h2>Author<h2>\n",
+    "    Kavana.N"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.10.9"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
